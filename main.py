6fe21178dfdc2ecdc126cefc7f87b1bf883396cc
Toolspeter <hua01jan@outlook.com>
2026-02-19 21:41:26 +0800

    add License

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# [main.py/Soveranima]
+#     Copyright (C) 2026  Toolspeter
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import dotenv
 import certifi
@@ -651,8 +664,6 @@ async def heartbeat_loop():
                     continue
 
             result = await loop.run_in_executor(None, client.soul.heartbeat, user_id)
-            if result and result.get('_pending_skill'):
-                result = await loop.run_in_executor(None, client.soul.continue_skill, result)
             if result and result.get("content"):
                 await send_long_message(channel, result["content"])
                 print(f"💓 主動關心 {user_id}: {result['content'][:50]}...")
--- prompts.py
+++ prompts.py
@@ -1,3 +1,16 @@
+# [promptps.py/Soveranima]
+#     Copyright (C) 2026  Toolspeter
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 獨立的 Prompt 檔案
 此檔案不會被 AI 讀取，確保 prompt 內容不會被自動修改
